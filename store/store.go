5d95ec4303ee14919712d3e16e6a871f5dc3a6c7
r <juroberttyb@gmail.com>
2025-10-13 16:07:33 +0800

    update: interface template

--- store/store.go
+++ store/store.go
@@ -2,8 +2,11 @@ package store
 
 import (
 	"context"
+
+	"github.com/A-pen-app/recommender_sdk/model"
 )
 
-type RecommendStore interface {
+type RecommendStore[T model.Rankable] interface {
 	NotifyStickiness(ctx context.Context, userID, postID string) error
+	NewRecommender(ctx context.Context, userID string) *Recommender[T]
 }
