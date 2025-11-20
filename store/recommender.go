84c5b6dfdbe96afffd5ff5da94b122616d91e051
r <juroberttyb@gmail.com>
2025-11-20 12:03:14 +0800

    update: rule based approach, non annonymous rule

--- store/recommender.go
+++ store/recommender.go
@@ -2,9 +2,7 @@ package store
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"net/http"
 	"sort"
 	"time"
 
@@ -54,8 +52,6 @@ func (r *recommendStore[T]) NotifyStickiness(ctx context.Context, userID, postID
 }
 
 const nonAnnonymousFactor float64 = 2.
-
-const recommenderURL string = "https://recommender-490242039522.asia-east1.run.app/recommendations/%s"
 
 type Recommender[T model.Rankable] struct {
 	weightCh <-chan map[string]float64
@@ -117,37 +113,8 @@ func (r *Recommender[T]) Recommend(ctx context.Context, candidates []T) {
 	sort.Sort(model.Rankables[T](candidates))
 }
 
-func buildRecommenderURL(userID string) string {
-	url := fmt.Sprintf(recommenderURL, userID)
-	return url
-}
-
 func getWeights(ctx context.Context, userID string) (map[string]float64, error) {
-	url := buildRecommenderURL(userID)
-	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
-	if err != nil {
-		logging.Infow(ctx, "failed creating request", "msg", err)
-		return nil, err
-	}
-
-	response, err := http.DefaultClient.Do(req)
-	if err != nil {
-		logging.Infow(ctx, "error performing request", "msg", err)
-		return nil, err
-	}
-	defer response.Body.Close()
-
-	if response.StatusCode != http.StatusOK {
-		logging.Infow(ctx, "recommender respond with unexpected status code", "status_code", response.Status)
-		return nil, err
-	}
-
-	var scores map[string]float64
-	if err := json.NewDecoder(response.Body).Decode(&scores); err != nil {
-		logging.Infow(ctx, "recommender error decoding score map", "msg", err)
-		return nil, err
-	}
-	logging.Debug(ctx, fmt.Sprintf("similarity %+v", scores))
+	var scores map[string]float64 = make(map[string]float64)
 
 	if stickiness := GetStickinessScore(ctx, userID); stickiness != nil {
 		logging.Infow(ctx, "stickiness cache retrieved", "score_length", len(stickiness.Scores))
